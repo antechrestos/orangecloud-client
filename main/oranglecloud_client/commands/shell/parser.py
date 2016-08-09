e67eeb8de3a6d2b3ce11c0a1951b5a0f10564332
Ben Einaudi <benjamin.einaudi@orange.com>
2016-08-09 21:15:37 +0200

    Character espace ok (space and so on)

--- main/oranglecloud_client/commands/shell/parser.py
+++ main/oranglecloud_client/commands/shell/parser.py
@@ -1,9 +1,32 @@
-
-
 def parse_line(line):
-    line = line.rstrip('\r\n').lstrip(' \t')
-    command_line_splitted = line.split(' ')
-    command_name = command_line_splitted[0]
-    parameters = tuple(command_line_splitted[1:])
-    return command_name, parameters
+    line = line.rstrip('\r\n').lstrip(' \t').replace('\t', ' ')
+    idx_first_space = line.find(' ')
+    if idx_first_space == -1:
+        return line, ()
+    else:
+        command_name = line[:idx_first_space]
+        parameters = line[idx_first_space:].lstrip(' ')
+        return command_name, _split_parameters(parameters)
+
+
+def _split_parameters(parameters):
+    result = []
+    escape_character = '\\'
+    split_character = ' '
+    previous_escape = False
+    current_parameter = []
+    for character in parameters:
+        if previous_escape or (character != escape_character and character != split_character):
+            current_parameter.append(character)
+            previous_escape = False
+        elif character == escape_character:
+            previous_escape = True
+        elif len(current_parameter) > 0:
+            result.append(''.join(current_parameter))
+            current_parameter = []
+    if len(current_parameter) > 0:
+        result.append(''.join(current_parameter))
+    print '_split_parameters - %s => %s' % (parameters, str(result))
+    return tuple(result)
+
 
--- test/orangecloud_client_test/commands/shell/test_parser.py
+++ test/orangecloud_client_test/commands/shell/test_parser.py
@@ -6,12 +6,35 @@ from oranglecloud_client.commands.shell.parser import parse_line
 class TestParser(unittest.TestCase):
     def test_simple_command(self):
         command, parameters = parse_line('cd somewhere')
-        self.assertIsNotNone(command)
-        self.assertIsNotNone(parameters)
         self.assertIsInstance(parameters, tuple)
         self.assertEqual(1, len(parameters))
         self.assertEqual('cd', command)
         self.assertEqual('somewhere', parameters[0])
 
-    def test_escaped_commands(self):
-        pass
+    def test_quoted_space(self):
+        command, parameters = parse_line('cd somewhere\ else')
+        self.assertIsInstance(parameters, tuple)
+        self.assertEqual(1, len(parameters))
+        self.assertEqual('cd', command)
+        self.assertEqual('somewhere else', parameters[0])
+
+    def test_quoted_command(self):
+        command, parameters = parse_line('cd "somewhere else"')
+        self.assertIsInstance(parameters, tuple)
+        self.assertEqual(1, len(parameters))
+        self.assertEqual('cd', command)
+        self.assertEqual('somewhere else', parameters[0])
+
+    def test_escaped_quote_command(self):
+        command, parameters = parse_line('cd somewhere\\"escaped')
+        self.assertIsInstance(parameters, tuple)
+        self.assertEqual(1, len(parameters))
+        self.assertEqual('cd', command)
+        self.assertEqual('somewhere"escaped', parameters[0])
+
+    def test_quoted_and_escaped_quote_command(self):
+        command, parameters = parse_line('cd "somewhere \\"escaped "')
+        self.assertIsInstance(parameters, tuple)
+        self.assertEqual(1, len(parameters))
+        self.assertEqual('cd', command)
+        self.assertEqual('somewhere "escaped ', parameters[0])
